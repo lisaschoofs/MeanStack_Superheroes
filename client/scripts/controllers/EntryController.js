8b94e64b94446f589b8cd829cbbffd63f24cf72f
Lisa Schoofs <lisaschoofs@gmail.com>
2017-07-26 21:56:42 -0500

    start of client side http calls

--- client/scripts/controllers/EntryController.js
+++ client/scripts/controllers/EntryController.js
@@ -1,3 +1,12 @@
 myApp.controller('EntryController', ['$http', '$location', 'HeroService', function($http, $location, HeroService){
   console.log('Entry controller loaded');
+
+  //POST route to add student to the database
+  $scope.addHero = function(hero) {
+    $http.post('/hero', hero).then(function(response){
+      console.log('back from the server with success!', response);
+      alert('you added a hero to the list!');
+      });
+    };
+
 }]);
--- client/scripts/factories/HeroService.js
+++ client/scripts/factories/HeroService.js
@@ -1,7 +1,18 @@
 myApp.factory('HeroService', ['$http', '$location', function($http, $location){
   console.log('Here Service loaded');
 
-  return {
+  //for list of heroes from DB
+  var heroList = {};
 
+  //gets all heroes from the database
+  function getLessons() {
+    $http.get('/hero').then(function(response){
+      heroList.heroes = response.data;
+    });
+  }
+
+  return {
+    getHeroes: getHeroes,
+    heroList: heroList    
   };
 }]);
